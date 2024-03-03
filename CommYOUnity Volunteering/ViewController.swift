b3dbcd9dee2165be93c02f4140952d5f1d426ac2
Adedeji Kuforiji <ragingmage1492@gmail.com>
2024-03-03 14:30:55 -0600

    Initial Commit

---
+++ CommYOUnity Volunteering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommYOUnity Volunteering
+//
+//  Created by Adedeji Jare Kuforiji on 3/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
