3f4984592cfb5a1970b808e37222833eb9725741
mstoakley <mstoakley@pvamu.edu>
2024-03-28 21:58:36 -0500

    Initial Commit

---
+++ commYOUnityCares/commYOUnityCaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  commYOUnityCaresApp.swift
+//  commYOUnityCares
+//
+//  Created by Mariah J Stoakley on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct commYOUnityCaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
