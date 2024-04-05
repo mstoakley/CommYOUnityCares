8ce96c9bfce552588dab6c7a2fa6ef77f9b6f5bb
Javon Thomas <89162381+JTKryptic@users.noreply.github.com>
2024-04-05 00:26:50 -0500

    Revert "Modified design of collectionView"

--- ViewController.swift
+++ ViewController.swift
@@ -9,56 +9,45 @@ import UIKit
 import SwiftUI
 
 
+import Foundation
+import UIKit
 
 
-class PostViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+
+class PostViewController: UIViewController
 
 {
-    var collectionView: UICollectionView!
-    var posts = [PostView]()
-    
-    
-    override func viewDidLoad()
-    {
-    
-        if (posts.count == 0)
-        {
-            view.backgroundColor = UIColor.systemGray6
-            let dpn = UILabel()
-            dpn.text = "No Posts Found"
-            dpn.font = UIFont(name: "SF Pro",size:25)
-            dpn.frame = CGRect(x:100,y:200,width:200, height: 25)
-            dpn.textAlignment = NSTextAlignment.center
-            view.addSubview(dpn)
-    
-        }
-        else{
-            let layout = UICollectionViewFlowLayout()
-            collectionView = UICollectionView(frame: .zero,collectionViewLayout: layout)
-            collectionView.backgroundColor = UIColor.systemGray6
-            view.addSubview(collectionView)
-            
-            collectionView.translatesAutoresizingMaskIntoConstraints = false
-            collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-            
-            
-            collectionView.dataSource = self
-            collectionView.delegate = self
-            collectionView.register(PostCell.self, forCellWithReuseIdentifier: "cell")
-        }
-    
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-    {
-        return posts.count
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
-    {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PostCell
-        return cell
+    @IBOutlet var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
 }
+
+
+extension PostViewController: UICollectionViewDelegate
+{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("Press Registered")
+    }
+}
+
+extension PostViewController: UICollectionViewDataSource
+{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        return cell
+    }
+}
+/*
+extension PostViewController: UICollectionViewDelegateFlowLayout
+{
+    
+}
+*/
