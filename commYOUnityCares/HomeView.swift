b03f5b849c2a224b45189988fcc7faeb7827af1d
mstoakley <mstoakley@pvamu.edu>
2024-04-05 02:38:58 -0500

    homeview

---
+++ commYOUnityCares/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  commYOUnityCares
+//
+//  Created by Mariah J Stoakley on 4/5/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
